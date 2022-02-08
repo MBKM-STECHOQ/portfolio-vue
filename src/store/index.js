37221d4520345fe4695ca0acdf54c5bbd0ea0ab0
taufiq <me@taufiqhdyt.com>
2022-02-08 14:23:38 +0700

    add site info data

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import data from '../services';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -9,6 +11,14 @@ export default new Vuex.Store({
     user: {
       name: 'John',
     },
+    // create Site Detail
+    siteInfo: {
+      title: 'Portfolio Vue',
+      description: 'This is a portfolio website',
+    },
+    project: [
+      ...data,
+    ],
   },
   mutations: {
     // 2. Create Mutations to manipulate State
@@ -23,6 +33,9 @@ export default new Vuex.Store({
     },
     // get Name from state
     getName: (state) => state.user.name,
+    // get Site Detail from state
+    getSiteInfo: (state) => state.siteInfo,
+    getProject: (state) => state.project,
   },
   actions: {
     // 3. Create Actions to trigger mutation
