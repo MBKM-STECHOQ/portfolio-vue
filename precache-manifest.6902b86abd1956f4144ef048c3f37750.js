93139044d3cf135f36bc2ed70471cc0ffdc6a24d
TaufiqHdyt <TaufiqHdyt@users.noreply.github.com>
2022-02-08 09:03:50 +0000

    deploy: 8842835b1fe2dac9444e7042340e9ce6514097e0

---
+++ precache-manifest.6902b86abd1956f4144ef048c3f37750.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f33521c70def2254f584",
+    "url": "/frontend/portofolio-vue/css/app.f9b1c236.css"
+  },
+  {
+    "revision": "e738030057f63351ff90",
+    "url": "/frontend/portofolio-vue/css/contact.cfbeb8db.css"
+  },
+  {
+    "revision": "8a0b6312ef4740ae2224",
+    "url": "/frontend/portofolio-vue/css/profile.de8da5f0.css"
+  },
+  {
+    "revision": "fa7b2d751851cd31f01c1bb4254214f8",
+    "url": "/frontend/portofolio-vue/index.html"
+  },
+  {
+    "revision": "8ce7a50881b9708c1976",
+    "url": "/frontend/portofolio-vue/js/about.33c9b457.js"
+  },
+  {
+    "revision": "f33521c70def2254f584",
+    "url": "/frontend/portofolio-vue/js/app.8e773458.js"
+  },
+  {
+    "revision": "ecda6f5c34b643e2aff3",
+    "url": "/frontend/portofolio-vue/js/chunk-vendors.287cc30c.js"
+  },
+  {
+    "revision": "e738030057f63351ff90",
+    "url": "/frontend/portofolio-vue/js/contact.896fae74.js"
+  },
+  {
+    "revision": "8a0b6312ef4740ae2224",
+    "url": "/frontend/portofolio-vue/js/profile.f11a4348.js"
+  },
+  {
+    "revision": "5591c04851ae28b5bfc2",
+    "url": "/frontend/portofolio-vue/js/user.281009d8.js"
+  },
+  {
+    "revision": "38c479585f3160952bf213e093146e27",
+    "url": "/frontend/portofolio-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/frontend/portofolio-vue/robots.txt"
+  }
+]);
