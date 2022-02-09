a591216e834f42434636c8e61c160778170ee373
taufiq <me@taufiqhdyt.com>
2022-02-09 14:36:38 +0700

    add image project dummy

--- src/views/DetailProject.vue
+++ src/views/DetailProject.vue
@@ -3,6 +3,7 @@
     <article v-if="projectById !== undefined">
       <h1>{{projectById.name}}</h1>
       <p>{{projectById.description}}</p>
+      <img :src="projectById.image" :alt="projectById.description">
     </article>
     <article v-else>
       <h1>Project Not Found</h1>
